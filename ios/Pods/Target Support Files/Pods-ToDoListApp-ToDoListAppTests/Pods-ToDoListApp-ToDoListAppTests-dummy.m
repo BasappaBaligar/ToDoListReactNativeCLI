cf29e659c49e2365930394cc7c35b60376b27951
BasappaBaligar <baligar.basavaraj@gmail.com>
2020-04-22 14:23:20 +0530

    Revert "adding all the files"
    This reverts commit 6ffe64032acec0157431d91a4c607cfd99604c49.

--- ios/Pods/Target Support Files/Pods-ToDoListApp-ToDoListAppTests/Pods-ToDoListApp-ToDoListAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ToDoListApp_ToDoListAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_ToDoListApp_ToDoListAppTests
-@end
--- ios/Pods/Target Support Files/Pods-ToDoListApp/Pods-ToDoListApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ToDoListApp : NSObject
-@end
-@implementation PodsDummy_Pods_ToDoListApp
-@end
